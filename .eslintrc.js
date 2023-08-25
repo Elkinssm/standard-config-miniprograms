02d6d7ea3cbd646bb3359bf90bd39ad36c27119a
ECM8833A <Ecm8833a@claro.com.co>
2023-08-24 23:13:34 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ["eslint:recommended", "prettier", "standard", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
+  },
+  globals: {
+    Page: "readonly",
+    App: "readonly",
+    Component: "readonly",
+    my: "readonly",
+    getApp: "writable"
+  },
+  parserOptions: {
+    ecmaVersion: 2022
+  },
+  rules: {
+    "no-var": "error",
+    "prefer-const": "error",
+    "no-unused-vars": "warn",
+    "no-undef": "error",
+    "no-console": "warn",
+    "no-debugger": "error",
+    "no-constant-condition": "error"
+  }
+};
---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+Page({
+  data: {
+    launchData: null,
+    showData: null
+  },
+
+  onLaunch(options) {
+    console.log(test);
+    console.log("Received data in onLaunch:", options);
+    const receivedData = options.query.data ? JSON.parse(decodeURIComponent(options.query.data)) : null;
+    console.log("Received data in onLaunch:", receivedData);
+    this.setData({
+      launchData: receivedData
+    });
+  },
+  onShow(options) {
+    console.log("Received data in onShow:", options);
+    const receivedData = options.query.data ? JSON.parse(decodeURIComponent(options.query.data)) : null;
+    console.log("Received data in onShow:", receivedData);
+    this.setData({
+      showData: receivedData
+    });
+  },
+
+  getParam() {
+    const {query, referrerInfo: {extraData} = {}} = my.getLaunchOptionsSync();
+    my.alert({
+      title: "Prueba data",
+      content: `query:${JSON.stringify(query) || ""}\nextraData:${JSON.stringify(extraData) || ""}`
+    });
+  }
+});
