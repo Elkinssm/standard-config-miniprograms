a8ea9dfd080ec199eb0afc0ef5b0c398e9e8e778
Elkinssm <elkinsilvamanrique@gmail.com>
2023-08-25 10:48:23 -0500

    first commit prettier and husky rules

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,7 +13,15 @@ Page({
       launchData: receivedData
     });
   },
+
+
+
+
+
+
+  
   onShow(options) {
+    var test = this
     console.log("Received data in onShow:", options);
     const receivedData = options.query.data ? JSON.parse(decodeURIComponent(options.query.data)) : null;
     console.log("Received data in onShow:", receivedData);
